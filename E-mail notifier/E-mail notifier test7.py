b62e7a1e302c598b82d3178986532201e14c9ddf
burakalbeni <71968376+burakalbeni@users.noreply.github.com>
2021-03-02 00:09:09 +0300

    Update E-mail notifier test7.py

--- E-mail notifier/E-mail notifier test7.py
+++ E-mail notifier/E-mail notifier test7.py
@@ -9,7 +9,7 @@ while 1:
  obj.select() #refresh 
  val = len(obj.search(None, 'UnSeen')[1][0].split()) #generate a value of 1 or 0
  print ("The val is: %d\n" %val) #print value to monitor
- if(val==0):#the case where we get the value '0' python recognizes it as a boolean false which is not intended a
+ if(val==0):#the case where we get the value '0' python recognizes it as a boolean false which is not intended 
   sercomm.write(b'0')
  sercomm.write(val) #write value to serial port
 #ser.close() 
